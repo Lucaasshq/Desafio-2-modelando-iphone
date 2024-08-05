ec0478adbfbe3c75e2dd983b8599707a42ceef0b
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-05 16:46:13 -0300

    completo

---
+++ src/Iphone/Iphone.java
@@ -0,0 +1,59 @@
+package Iphone;
+
+import Internet.Internet;
+import MP3.MP3;
+import Telefone.Ramal;
+
+public class Iphone implements Internet, MP3, Ramal {
+
+    @Override
+    public void browser() {
+        System.out.println("Acessando browser via Iphone.Iphone");
+
+    }
+
+    @Override
+    public void email() {
+        System.out.println("Acessando email via Iphone.Iphone");
+    }
+
+    @Override
+    public void googleMaps() {
+        System.out.println("Acessando google maps via Iphone.Iphone");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando musica via Iphone.Iphone");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando musica via Iphone.Iphone");
+    }
+
+    @Override
+    public void SelecionarMusica() {
+        System.out.println("Selecionando musica via Iphone.Iphone");
+    }
+
+    @Override
+    public void ligar() {
+        System.out.println("Ligando via Iphone.Iphone");
+    }
+
+    @Override
+    public void desligar() {
+        System.out.println("Desligando via Iphone.Iphone");
+    }
+
+    @Override
+    public void selecionarContato() {
+        System.out.println("Selecionando contato via Iphone.Iphone");
+    }
+
+    @Override
+    public void realizarChamada() {
+        System.out.println("Realizando chamada via Iphone.Iphone");
+    }
+}
---
+++ src/Usuario.java
@@ -0,0 +1,19 @@
+import Iphone.Iphone;
+
+public class Usuario {
+
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone();
+
+        iphone.browser();
+        iphone.email();
+        iphone.googleMaps();
+        iphone.ligar();
+        iphone.realizarChamada();
+        iphone.selecionarContato();
+        iphone.desligar();
+        iphone.SelecionarMusica();
+        iphone.tocar();
+        iphone.pausar();
+    }
+}
